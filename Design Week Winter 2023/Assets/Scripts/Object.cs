3f50c5003af79812c5b36bb3411e9d5ab99f596a
Axel Vera Jimenez <63276968+FlickrAV@users.noreply.github.com>
2023-03-07 12:12:28 -0500

    Added wonky crushing system

---
+++ Design Week Winter 2023/Assets/Scripts/Object.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    private bool againstPress = false;
+    private bool againstBelt = false;
+    private Animator anim;
+    public int animSpeed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(againstBelt && againstPress)
+            animSpeed = 1;
+        else
+            animSpeed = 0;
+
+        anim.speed = animSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.name == "Press")    
+        {
+            againstPress = true;
+        }
+
+        if(other.gameObject.name == "Conveyor Belt")    
+        {
+            againstBelt = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) 
+    {
+        if(other.gameObject.name == "Press")    
+        {
+            againstPress = false;
+        }
+
+        if(other.gameObject.name == "Conveyor Belt")    
+        {
+            againstBelt = false;
+        }
+    }
+}
