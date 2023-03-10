500f69cfb7c7ef6df52e54b6116d700c35f612fa
Axel Vera Jimenez <63276968+FlickrAV@users.noreply.github.com>
2023-03-10 02:17:48 -0500

    done i think

--- Design Week Winter 2023/Assets/Scripts/GameManager.cs
+++ Design Week Winter 2023/Assets/Scripts/GameManager.cs
@@ -6,9 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> objects;
     public int money;
+    public GameObject luigiPrefab;
 
     public void SpawnNewObject()
     {
         Instantiate(objects[Random.Range(0, objects.Count)], new Vector3(15, 0, 0), Quaternion.identity);
+        if(objects.Count == 8)
+        {
+            objects.Add(luigiPrefab);
+        }
+        Debug.Log(objects.Count);
     }
 }
